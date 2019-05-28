2a5d3c3a30f24c58ab42f7fed716003acbf4c709
Vítězslav Dvořák <info@vitexsoftware.cz>
2019-05-28 19:58:21 +0200

    Debian package composer fix

--- debian/fixversion.sh
+++ debian/fixversion.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 VERSTR=`dpkg-parsechangelog --show-field Version`
 COMPOSER_VERSTR=`echo ${VERSTR}|sed 's/-/./g'`
-echo update debian/php-subreg/usr/share/php/Ease/composer.json version to ${COMPOSER_VERSTR}
+echo update debian/php-subreg/usr/share/php/Subreg/composer.json version to ${COMPOSER_VERSTR}
 sed -i -e '/\"version\"/c\    \"version\": \"'${COMPOSER_VERSTR}'",' debian/php-subreg/usr/share/php/Ease/composer.json
 echo Update debian/php-subreg/usr/share/php/Ease/Atom.php version to ${VERSTR}
 sed -i -e "/static public \$libVersion/c\    static public \$frameworkVersion = '${VERSTR}';" debian/php-subreg/usr/share/php/Subreg/Client.php
