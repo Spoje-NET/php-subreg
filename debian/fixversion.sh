e7814dbdba6a728adb5c6415bb146da448b0209c
Vítězslav Dvořák <info@vitexsoftware.cz>
2021-09-05 22:58:46 +0200

    try to rename all to php-spojenet-subreg

--- debian/fixversion.sh
+++ debian/fixversion.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 VERSTR=`dpkg-parsechangelog --show-field Version`
 COMPOSER_VERSTR=`echo ${VERSTR}|sed 's/-/./g'`
-echo update debian/php-subreg/usr/share/php/Subreg/composer.json version to ${COMPOSER_VERSTR}
+echo update debian/php-spojentet-subreg/usr/share/php/Subreg/composer.json version to ${COMPOSER_VERSTR}
 sed -i -e '/\"version\"/c\    \"version\": \"'${COMPOSER_VERSTR}'",' debian/php-subreg/usr/share/php/Ease/composer.json
 echo Update debian/php-subreg/usr/share/php/Subreg/Client.php version to ${VERSTR}
-sed -i -e "/static public \$libVersion/c\    static public \$libVersion = '${VERSTR}';" debian/php-subreg/usr/share/php/Subreg/Client.php
+sed -i -e "/static public \$libVersion/c\    static public \$libVersion = '${VERSTR}';" debian/php-spojenet-subreg/usr/share/php/Subreg/Client.php
 echo Update src/Ease/Atom.php version to ${VERSTR}
 sed -i -e "/static public \$libVersion/c\    static public \$libVersion = '${VERSTR}';" src/Subreg/Client.php
